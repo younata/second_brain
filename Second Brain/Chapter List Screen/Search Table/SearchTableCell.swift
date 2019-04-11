70d0c1952be753bd0325df60c76d1769e7bd22f5
Rachel Brindle <rbrindle@opentable.com>
2019-04-11 09:09:55 -0700

    Begin looking at adding search

---
+++ Second Brain/Chapter List Screen/Search Table/SearchTableCell.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class SearchTableCell: UITableViewCell {
+
+}
---
+++ Second Brain/Chapter List Screen/Search Table/SearchTableDeleSource.swift
@@ -0,0 +1,9 @@
+//
+//  SearchTableDeleSource.swift
+//  Second Brain
+//
+//  Created by Rachel Brindle on 4/10/19.
+//  Copyright © 2019 Rachel Brindle. All rights reserved.
+//
+
+import Foundation
