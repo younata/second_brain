145fbad3c0af13d9238e906609841de847af90ca
Rachel Brindle <you@subluminal.net>
2019-07-06 10:31:02 -0700

    Remove mac version

--- Second Brain MacTests/Helpers/FakeURLOpener.swift
+++
@@ -1,9 +0,0 @@
-@testable import Second_Brain
-
-final class FakeURLOpener: URLOpener {
-    private(set) var openedURLs: [URL] = []
-    @discardableResult func open(_ url: URL) -> Bool {
-        self.openedURLs.append(url)
-        return true
-    }
-}
